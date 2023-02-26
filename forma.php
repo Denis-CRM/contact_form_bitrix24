14d79e5a3d5263536749b58b02302096e42e699b
Denis-CRM <61589635+Denis-CRM@users.noreply.github.com>
2023-02-27 00:02:38 +0200

    Create forma.php

---
+++ forma.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title>Форма входа</title>
+</head>
+<body>
+<form action="/lead_add_box.php" method="get">
+    <label>
+    Имя лида<input type="text" name="lead_name">
+    </label>
+    <br>
+    <label>
+    Телефон <input type="text" name="phone">
+    </label>
+    <br>
+    <label>
+    Почтовый ящик <input type="text" name="e-mail">
+    </label>
+    <br>
+    <label>
+    Комментарий <input type="text" name="comment">
+    </label>
+    <br>
+    <input type="submit" value="Создать лид">
+</form>
+</body>
+</html>
